6059765b18d291c8c7404e0fd5d04d5e3ac7f642
Vitor Bisterso dos Santos <vitor.santos@sensedia.com>
2022-07-11 10:14:19 -0300

    Add project without tests or routing

---
+++ pdp/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import "remixicon/fonts/remixicon.css";
+import "./index.scss";
+
+import PDPContent from "./PDPContent"
+import Header from "home/Header"
+import Footer from "home/Footer"
+
+const App = () => (
+  <Router>
+    <div className="text-3xl mx-auto max-w-6xl">
+      <Header />
+      <div className="my-10">
+        <Routes>
+          <Route path="/product/:id" element={<PDPContent />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  </Router>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
